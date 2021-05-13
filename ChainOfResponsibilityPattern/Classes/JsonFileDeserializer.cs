58de86cd23994813ebb721dc973b1c7946c88fc5
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-14 01:30:00 +0300

    Реализация класса - валидатора для JSON объектов

---
+++ ChainOfResponsibilityPattern/Classes/JsonFileDeserializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace ChainOfResponsibilityPattern.Classes
+{
+    /// <summary>
+    /// Реализует валидацию передаваемой строки как JSON объекта
+    /// </summary>
+    public class JsonFileDeserializer : BaseFileDeserializer
+    {
+        /// <summary>
+        /// Валидация JSON объекта
+        /// </summary>
+        /// <param name="request">Входной объект. Ожидается строка</param>
+        public override object Handle(object request)
+        {
+            string fileContent = request as string;
+            try
+            {
+                var obj = JToken.Parse(fileContent);
+                
+                Console.WriteLine("Обработчик JSON получил файл");
+                SaveFile(fileContent, "json");
+                
+                return fileContent;
+            }
+            catch (Exception)
+            {
+                return base.Handle(request);    
+            }
+        }
+    }
+}
