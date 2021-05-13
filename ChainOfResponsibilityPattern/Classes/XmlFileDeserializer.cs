c796188060e23598613c017c1e66b1c9ba77408a
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-14 01:30:13 +0300

    Реализация класса - валидатора для XML документов

---
+++ ChainOfResponsibilityPattern/Classes/XmlFileDeserializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ChainOfResponsibilityPattern.Classes
+{
+    /// <summary>
+    /// Реализует валидацию передаваемой строки как XML документа
+    /// </summary>
+    public class XmlFileDeserializer : BaseFileDeserializer
+    {
+        /// <summary>
+        /// Валидация XML документа
+        /// </summary>
+        /// <param name="request">Входной объект. Ожидается строка</param>
+        public override object Handle(object request)
+        {
+            string fileContent = request as string;
+            try
+            {
+                XDocument xml = XDocument.Parse(fileContent ?? string.Empty);
+                Console.WriteLine("Обработчик XML получил файл");
+                SaveFile(fileContent, "xml");
+                return fileContent;
+            }
+            catch (Exception)
+            {
+                return base.Handle(request);    
+            }
+        }
+    }
+}
