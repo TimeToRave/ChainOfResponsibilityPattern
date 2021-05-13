4c338d2b21855107afe6a943ca7411442e4b1006
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-14 01:29:29 +0300

    Создание интерфейса для обработчика и реализация базового класса-валидатора файла

---
+++ ChainOfResponsibilityPattern/Classes/BaseFileDeserializer.cs
@@ -0,0 +1,45 @@
+﻿using ChainOfResponsibilityPattern.Interfaces;
+
+namespace ChainOfResponsibilityPattern.Classes
+{
+    /// <summary>
+    /// Реализует последовательную проверку содержимого файла
+    /// для распознания его формата 
+    /// </summary>
+    public class BaseFileDeserializer : IHandler
+    {
+        /// <summary>
+        /// Следуюший обработчик
+        /// </summary>
+        public IHandler NextHandler { get; set; }
+
+        /// <summary>
+        /// Выполняет обработку входного файла
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public virtual object Handle(object request)
+        {
+            if (NextHandler != null)
+            {
+                return NextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Выполняет сохранение содержимого
+        /// файла с заданным расширением
+        /// </summary>
+        /// <param name="fileContent">Содержимое файла</param>
+        /// <param name="fileExtension">Расширение выходного файла</param>
+        public void SaveFile(string fileContent, string fileExtension)
+        {
+            FileOperator fileOperator = new FileOperator();
+            fileOperator.WriteTextToFile("output."+fileExtension, fileContent);
+        } 
+    }
+}
---
+++ ChainOfResponsibilityPattern/Interfaces/IHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace ChainOfResponsibilityPattern.Interfaces
+{
+    public interface IHandler
+    {
+        public IHandler NextHandler { get; set; }
+
+        public object Handle(object request);
+    }
+}
