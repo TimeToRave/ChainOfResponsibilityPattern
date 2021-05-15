a7a09dad0a498816401604b34137477d7a378c1a
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-15 14:11:21 +0300

    Реализация чтения входных файлов и отправки из на валидацию

---
+++ ChainOfResponsibilityPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using ChainOfResponsibilityPattern.Classes;
+
+namespace ChainOfResponsibilityPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = new Dictionary<string, string>();
+            PrepareFiles(args, files);
+            
+            FileDeserializationClient client = new FileDeserializationClient();
+            
+            foreach (var file in files)
+            {
+                client.Run(file.Key, file.Value);
+            }
+        }
+
+        private static void PrepareFiles(string[] filePathes, Dictionary<string, string> files)
+        {
+            if (filePathes.Length == 0)
+            {
+                files.Add("testxml",
+                    "<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>");
+                files.Add("testjson", "{\"data\":\"Click Here\",\"size\":36,\"style\":\"bold\"}");
+                files.Add("testcsv", "Иванов,25\nПетров,30");
+            }
+            else
+            {
+                FileOperator fileOperator = new FileOperator();
+                foreach (string filePath in filePathes)
+                {
+                    files.Add(filePath, fileOperator.ReadTextFromFile(filePath));
+                }
+            }
+        }
+    }
+}
