50c99230f3f4833874424d5fbecf2ae54b9018b9
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-05-15 14:11:02 +0300

    Добавлен класс - клиент для валидации содержимого файла

---
+++ ChainOfResponsibilityPattern/Classes/FileDeserializationClient.cs
@@ -0,0 +1,27 @@
+﻿using ChainOfResponsibilityPattern.Interfaces;
+
+namespace ChainOfResponsibilityPattern.Classes
+{
+    /// <summary>
+    /// Класс для валидации содержимого входного файла
+    /// </summary>
+    public class FileDeserializationClient
+    {
+        /// <summary>
+        /// Последовательный запуск обработчиков цепочки последовательности
+        /// </summary>
+        /// <param name="fileName">Название файла</param>
+        /// <param name="fileContent">Содержимое файла</param>
+        public void Run(string fileName, string fileContent)
+        {
+            IHandler xmlDeserializer = new XmlFileDeserializer();
+            IHandler jsonDeserializer = new JsonFileDeserializer();
+            IHandler csvDeserializer = new CsvFileDeserializer();
+
+            jsonDeserializer.NextHandler = csvDeserializer;
+            xmlDeserializer.NextHandler = jsonDeserializer;
+            
+            xmlDeserializer.Handle(fileName, fileContent);
+        }
+    }
+}
